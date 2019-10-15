338c29302972e8cb08a7a51f98e2bdb068b09f99
Raman Verma <ramanverma693@gmail.com>
2019-10-15 17:15:34 +0530

    task1 completed Add further changes here

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
+import { AppRegistry } from 'react-native';
+import Task1 from './src/Task1';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Task1);
---
+++ src/Task1.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text
+} from 'react-native';
+
+export default class Task1 extends Component {
+    render() {
+        return (
+            <SafeAreaView>
+                <Text>Hello world!</Text>
+            </SafeAreaView >
+        );
+    }
+};
+
+const styles = StyleSheet.create({
+    
+});
