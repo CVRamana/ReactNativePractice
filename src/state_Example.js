60ed281ad8b32adf9496e6432515c436477501f2
Raman Verma <ramanverma693@gmail.com>
2019-10-17 13:34:38 +0530

    Login complete assignment

--- src/state_Example.js
+++ src/state_Example.js
@@ -1,31 +1,51 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import {
     View,
     TextInput,
     Text,
-    
+
 } from 'react-native'
-export default class StateManagement extends Component{
-    constructor(props){
+export default class StateManagement extends Component {
+    constructor(props) {
         super(props)
-        this.state={
-            input:''
+        this.state = {
+            name: '',
+            email: "",
         }
 
     }
-    handleChangeInput=(text)=>{this.setState({input:text})
+    handleChangeInput = (key, val) => {
+        this.setState({ [key]: val })
 
     }
-    render(){
-        const {input} = this.state
+    render() {
+        const { input } = this.state
         return (
-            <View style={{flex:1, justifyContent:"center",alignItems:"center",flexDirection:"column"}}>
-                <TextInput style={{height:40 ,borderColor:"grey" ,borderWidth:1,width:300}}
-                onChangeText={this.handleChangeInput}
-                value={input}
-                />
-                <Text>Hello how r u?? {this.state.input}</Text>
+            <View style={{ flex: 1, alignItems: "center" }}>
+                <View style={{ marginTop: 50, paddingTop: 10, justifyContent:"center",alignItems:"center"}}>
+                    <Text style={{marginBottom:30,fontSize:30}}>Login Form !!</Text>
+                    <TextInput
+                        style={{ height: 40, borderColor: "#000", borderWidth: 1, width: 300, paddingLeft: 5, placeholderColor: "#000" }}
+                        onChangeText={(val) => this.handleChangeInput('name', val)}
+                        value={this.state.name}
+                        placeholder="Name"
+                    />
+                     <Text>
+                        {this.state.name}
+                    </Text>
                 </View>
+                <View style={{ marginTop: 10, padding: 10 }}>
+                    <TextInput
+                        style={{ height: 40, borderColor: "#000", borderWidth: 1, width: 300, paddingLeft: 5, placeholderColor: "#000" }}
+                        onChangeText={(val) => this.handleChangeInput('email', val)}
+                        value={this.state.email}
+                        placeholder="Email"
+                    />
+                    <Text style={{marginBottom:20}}>
+                        {this.state.email}
+                    </Text>
+                </View>
+            </View>
         );
     }
 }
